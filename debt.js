0622365414367e2f5e9d8360028fd97e2eb4a73f
mainUser <timwparkes@yahoo.com>
2019-08-11 02:39:45 -0400

    create landing page

---
+++ debt.js
@@ -0,0 +1,177 @@
+var holder = document.getElementById('holder');
+var button = document.getElementById('button');
+
+
+
+
+   button.addEventListener('click', function(){
+   	var rev = document.getElementById('rev').value;
+   	var rent = document.getElementById('rent').value;
+   	var cnote = document.getElementById('cnote').value;
+   	var pbill = document.getElementById('pbill').value;
+   	var misc = document.getElementById('misc').value;
+  	
+  	 rent = parseInt(rent);
+  	 cnote = parseInt(cnote);
+  	 pbill = parseInt(pbill);
+  	 misc = parseInt(misc);
+  	
+  	var parseVal = (rent + cnote + pbill + misc) / 4;
+
+   	
+   	var wklySV = rev - parseVal;
+   	 var mnthR = rent / 4;
+   	 var mnthCR = cnote / 4;
+   	 var mnthPH = pbill / 4;
+   	 var mnthMS = misc / 4;
+
+
+
+
+   	//var displayTotal = document.getElementById('rv');
+   //	displayTotal.textContent = `$${total}`;
+   	var rt = document.getElementById('rt');
+   	rt.textContent = `$${mnthR}`;
+   	var cr = document.getElementById('cr');
+   	cr.textContent = `$${mnthCR}`;
+   	var ph = document.getElementById('ph');
+   	ph.textContent = `$${mnthPH}`;
+   	var ms = document.getElementById('ms');
+   	ms.textContent = `$${mnthMS}`;
+   	var sv = document.getElementById('sv');
+   	sv.textContent = `$${wklySV}`;
+
+
+
+
+});
+
+
+//display
+
+var tblCont = document.getElementById('dp');
+var table = document.createElement('table');
+table.classList.add("table");
+table.classList.add("table-striped");
+
+table.innerHTML = 
+	`<thead>
+		<tr>
+        	
+        	<th>Rent</th>
+        	<th>Car Payment</th>
+        	<th>Phone Bill</th>
+        	<th>Misc</th>
+      	</tr>
+      </thead>
+ 		<tbody>
+      	<tr>
+        	
+        	<td id="rt">$0.00</td>
+        	<td id="cr">$0.00</td>
+        	<td id="ph">$0.00</td>
+        	<td id="ms">$0.00</td>
+      	</tr>
+       </tbody>`;
+
+ 
+    tblCont.appendChild(table);
+
+    var wklyGross = document.createElement('table');
+    wklyGross.classList.add("table");
+    wklyGross.classList.add("table-striped");
+    wklyGross.classList.add("wklyGross");
+    wklyGross.innerHTML =
+    `<thead>
+		<tr>        	
+        	<th>What you should Save per week</th>        	
+      	</tr>
+      </thead>
+      <tbody>
+      	<tr>        	
+        	<td id="sv">$0.00</td>
+       	</tr>
+       </tbody>`;
+    
+    tblCont.appendChild(wklyGross);
+
+
+    //buttons///
+    var crBtn = document.getElementById('crBtn');
+    var ccProfile = document.getElementById('ccProfile');
+    var sbProfile = document.getElementById('sbProfile');
+    var lgBtn = document.getElementById('lgBtn');
+    var fgtPass = document.getElementById('fgtPass');
+   
+
+
+    var lndPage = document.querySelector('.landing-pg');
+    var bg = document.querySelector('.bg');
+    var crProfile = document.getElementById('crProfile');
+    var lndAction = document.getElementById('lndAction');
+    var pgHeader = document.getElementById('pgHeader');
+    var lgSubmit = document.getElementById('lgSubmit');
+
+ 
+
+
+    crBtn.addEventListener('click', createProfile);
+    ccProfile.addEventListener('click', cancelProfile);
+    sbProfile.addEventListener('click', submitProfile);
+    lgBtn.addEventListener('click', login);
+    lgSubmit.addEventListener('click', goHome);
+
+
+    
+
+   
+
+
+
+
+
+    function hidePage(p){
+      p.style.display = 'none';
+    }
+    function showPage(s){
+      s.style.display = 'block';
+    }
+
+
+    function createProfile(){  
+      crProfile.style.display = 'block';      
+      hidePage(lndAction);
+      pgHeader.textContent = 'create your profile'  
+    }
+
+
+    function cancelProfile(){
+      hidePage(crProfile);      
+      showPage(lndAction);
+      pgHeader.textContent = 'Hello, welcome to the app' 
+    }
+
+    function submitProfile(){
+      hidePage(lndPage);
+      bg.style.display = 'grid';
+    }
+
+    function login(){
+
+      showPage(user);
+       hidePage(lndAction); 
+       
+
+    }
+
+    function forgotPassword(){
+      showPage(crProfile);      
+      hidePage(lndAction);
+    }
+
+    function goHome(){
+      hidePage(lndPage);
+      bg.style.display = 'grid';
+    }
+
+   
